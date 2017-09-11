2588ddc237612a0b3a54f08a98deba6e743ec3b8
Bruno Dias <dias.h.bruno@gmail.com>
2017-09-10 21:15:52 -0300

    feature: update(f, k, o).
    update the value for key 'k' if present.
    function 'f' will receive the current value and must return
    the new one.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-
+export function update(f, k, o) {
+  if (!o[k]) return o;
+  o[k] = f(o[k]);
+  return o;
+}
--- tests/index.js
+++ tests/index.js
@@ -1,4 +1,11 @@
 import * as O from '../src/index.js';
 
 describe("objects", () => {
+  it("update(f, k, o)", () => {
+    const obj = { a: "a", c: 1 };
+    O.update(v => v + "b", "a", obj).should.be.eql({
+      a: "ab",
+      c: 1
+    });
+  });
 });
