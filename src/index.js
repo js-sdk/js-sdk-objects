c0471af4a4a18daa4a205fa52a3bb215f3a66092
Bruno Dias <dias.h.bruno@gmail.com>
2017-09-12 00:14:03 -0300

    feature: merge(f, a, b).
    to make it recursive, just check if the values
    that the function receives are Objects and call
    merge on them.

--- src/index.js
+++ src/index.js
@@ -9,3 +9,23 @@ export function updateWithKey(f, k, o) {
   o[k] = f(k, o[k]);
   return o;
 }
+
+export function merge(f, a, b) {
+  let unique = [];
+  return Object.keys(a).concat(Object.keys(b)).reduce(
+    (acc, k) => {
+      if (unique.indexOf(k) == -1) {
+        unique.push(k);
+        // this is a fusion of unique and the merge algorithm.
+        if (a[k] && b[k]) {
+          acc[k] = f(k, a[k], b[k]);
+        } else {
+          if (a[k]) acc[k] = a[k];
+          else acc[k] = b[k];
+        }
+      }
+      return acc;
+    },
+    {}
+  );
+}
