ce5a6b82214e99020a41b4b73acb32a1d94a0eb0
Bruno Dias <dias.h.bruno@gmail.com>
2017-09-12 00:24:24 -0300

    Release 0.4.1.

--- changelog.md
+++ changelog.md
@@ -1,3 +1,10 @@
+0.4.1 - Tue, 12 Sep 2017 00:24:37 UTC
+-------------------------------------
+
+- [c0471af](../../commit/c0471af) feature: merge(f, a, b).
+- [f10ac9c](../../commit/f10ac9c) chore: fix documentation style.
+
+
 0.3.1 - Sun, 10 Sep 2017 21:39:57 UTC
 -------------------------------------
 
--- dist/objects.js
+++ dist/objects.js
@@ -18,6 +18,7 @@
   });
   exports.update = update;
   exports.updateWithKey = updateWithKey;
+  exports.merge = merge;
   function update(f, k, o) {
     if (!o[k]) return o;
     o[k] = f(o[k]);
@@ -28,5 +29,21 @@
     if (!o[k]) return o;
     o[k] = f(k, o[k]);
     return o;
+  }
+
+  function merge(f, a, b) {
+    var unique = [];
+    return Object.keys(a).concat(Object.keys(b)).reduce(function (acc, k) {
+      if (unique.indexOf(k) == -1) {
+        unique.push(k);
+        // this is a fusion of unique and the merge algorithm.
+        if (a[k] && b[k]) {
+          acc[k] = f(k, a[k], b[k]);
+        } else {
+          if (a[k]) acc[k] = a[k];else acc[k] = b[k];
+        }
+      }
+      return acc;
+    }, {});
   }
 });
